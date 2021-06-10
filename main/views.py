250eadc61542a51128d4154b1921e8ccd496cbad
harsh9051 <jain.hrsh01@gmail.com>
2021-06-10 16:58:02 +0530

    first commit

---
+++ main/views.py
@@ -0,0 +1,189 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def blog1(request):
+    return render(request,'blog-1.html')
+
+def blog2(request):
+    return render(request,'blog-2.html')
+
+def blogcard(request):
+    return render(request,'blog-card.html')
+
+def blogsingle1(request):
+    return render(request,'blog-single-1.html')
+
+def blogsingle(request):
+    return render(request,'blog-single.html')
+
+def bookdescription(request):
+    return render(request,'book-description.html')
+
+def book(request):
+    return render(request,'book.html')
+
+def colors(request):
+    return render(request,'colors.html')
+
+def coursecard(request):
+    return render(request,'course-card.html')
+
+def courseintro(request):
+    return render(request,'course-intro.html')
+
+def courselist(request):
+    return render(request,'course-list.html')
+
+def coursepathlevel(request):
+    return render(request,'course-path-level.html')
+
+def coursepath(request):
+    return render(request,'course-path.html')
+
+def courseresume(request):
+    return render(request,'course-resume.html')
+
+def courses(request):
+    return render(request,'courses.html')
+
+def cover(request):
+    return render(request,'cover.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def developmentelements(request):
+    return render(request,'development-elements.html')
+
+def developmenticons(request):
+    return render(request,'development-icons.html')
+
+def dropdown(request):
+    return render(request,'dropdown.html')
+
+def elementsgrid(request):
+    return render(request,'elements-grid.html')
+
+def flex(request):
+    return render(request,'flex.html')
+
+def formlogin(request):
+    return render(request,'form-login.html')
+
+def formmodernlogin(request):
+    return render(request,'form-modern-login.html')
+
+def grid(request):
+    return render(request,'grid.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def index(request):
+    return render(request,'index.html')
+
+def inverse(request):
+    return render(request,'inverse.html')
+
+def pagecomponents(request):
+    return render(request,'page-components.html')
+
+def pagecontact(request):
+    return render(request,'page-contact.html')
+
+def pageelements(request):
+    return render(request,'page-elements.html')
+
+def pageicons(request):
+    return render(request,'page-icons.html')
+
+def pagelogin(request):
+    return render(request,'page-login.html')
+
+def pagepricing(request):
+    return render(request,'page-pricing.html')
+
+def pageprivacy(request):
+    return render(request,'page-Privacy.html')
+
+def pageprivecy(request):
+    return render(request,'page-privecy.html')
+
+def pageregister(request):
+    return render(request,'page-register.html')
+
+def pagesimplelogin(request):
+    return render(request,'page-simple-login.html')
+
+def pagesimpleregister(request):
+    return render(request,'page-simple-register.html')
+
+def pageterm(request):
+    return render(request,'page-term.html')
+
+def pagesterms(request):
+    return render(request,'pages-terms.html')
+
+def profile1(request):
+    return render(request,'profile-1.html')
+
+def profileedit(request):
+    return render(request,'profile-edit.html')
+
+def slider(request):
+    return render(request,'slider.html')
+
+def slideshow(request):
+    return render(request,'slideshow.html')
+
+def specialitycommingsoon(request):
+    return render(request,'speciality-comming-soon.html')
+
+def tabs(request):
+    return render(request,'tabs.html')
+
+def takecourse(request):
+    return render(request,'takecourse.html')
+
+def transition(request):
+    return render(request,'transition.html')
+
+def userprofile1(request):
+    return render(request,'user-profile-1.html')
+
+def userprofileedit(request):
+    return render(request,'user-profile-edit.html')
+
+def userprofile(request):
+    return render(request,'user-profile.html')
+
+def utility(request):
+    return render(request,'utility.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
